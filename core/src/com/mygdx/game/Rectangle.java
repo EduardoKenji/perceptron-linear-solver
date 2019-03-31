59c7132c4ac9df89033bc0baf6b86654a2a50f31
Eduardo K. M. Koketu <eduardo.koketu@gmail.com>
2019-03-30 21:52:43 -0300

    Initial commit

---
+++ core/src/com/mygdx/game/Rectangle.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+public class Rectangle {
+    float x, y, width, height;
+
+    public Rectangle(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+}
